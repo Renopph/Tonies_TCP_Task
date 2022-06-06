6f95e073d1ca8b648580d65441bf310ecbd52d03
Renopph <rene@arimond.de>
2022-06-06 10:21:47 +0200

    now able to send and receive messages

--- tcp_app/client.py
+++ tcp_app/client.py
@@ -2,24 +2,35 @@
 import socket
 
 
-class Client():
-    """A simple client to connect to the server and send messages."""
+# class Client():
+#     """A simple client to connect to the server and send messages."""
 
-    def __init__(self,port):
-        self.host = socket.gethostname()
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#     def __init__(self,port):
+#         self.host = socket.gethostname()
+#         self.port = port
+#         # SOCK_STREAM -> TCP socket
+#         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         
-    def connect_to_server(self):
-        self.socket.connect((self.host,self.port))
-        self.socket.sendall(b'Hello World')
-        data = self.socket.recv(1024)
+#     def connect_to_server(self):
+#         self.socket.connect((self.host,self.port))
+#         self.socket.sendall(b'Hello World')
+#         data = self.socket.recv(1024)
 
 
 HOST = 'localhost'
-PORT = 12345
+PORT = 9999
+data=b'Hello World'
 
-if __name__ == '__main__':
-    client = Client(PORT)
+# if __name__ == '__main__':
+#     client = Client(PORT)
 
-    client.connect_to_server()
+#     client.connect_to_server()
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+    sock.connect((HOST,PORT))
+    sock.sendall(bytes(str(data) + '\n','utf-8'))
+
+    received = str(sock.recv(1024),'utf-8')
+
+print('Sent: {}'.format(data))
+print('Received: {}'.format(received))
--- tcp_app/server.py
+++ tcp_app/server.py
@@ -1,32 +1,37 @@
-import socket
+import socketserver
 
-class Server():
+class Server(socketserver.BaseRequestHandler):
     """A simple server that accepts client connections."""
 
-    def __init__(self,host,port):
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.socket.bind((host,port))     
+    def handle(self):
+        """Test handler. This function needs to be overwritten in order for the communication to be customized."""
+        self.data = self.request.recv(1024).strip()
+        print('{} wrote:'.format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(self.data.upper())  
         
-    def run_server(self):
-        """Tries to listen for client connections and receive data continuously."""
-        self.socket.listen(1)
-        connection, address = self.socket.accept()
-        while True:
-            try:
-                data = connection.recv(1024)
+    # def run_server(self):
+    #     """Tries to listen for client connections and receive data continuously."""
+    #     self.socket.listen(1)
+    #     connection, address = self.socket.accept()
+    #     while True:
+    #         try:
+    #             data = connection.recv(1024)
 
-                if not data:break
-                print('Client says:' + data)
-                connection.sendall('Server says:hi')
-            except socket.error as e:
-                print('Error occurred:' + str(e))
-                break
-        connection.close()
+    #             if not data:break
+    #             print('Client says:' + data)
+    #             connection.sendall('Server says:hi')
+    #         except socket.error as e:
+    #             print('Error occurred:' + str(e))
+    #             break
+    #     connection.close()
 
 HOST = 'localhost'
-PORT = 12345
+PORT = 9999
 
 if __name__ == '__main__':
 
-    server = Server(HOST,PORT)
-    server.run_server()
+    # Create server at the defined port on localhost
+    with socketserver.TCPServer((HOST,PORT),Server) as server:
+        # This will keep the server running until someone closes it (CTRL+C in the Linux terminal)
+        server.serve_forever()
